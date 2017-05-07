5e944854d64dd2ba5569efc7caa1aee1bbec6704
Leon Archer <leon_archer@mail.ru>
2017-05-08 03:04:29 +0700

    Fully Spark-compatible algorithm implemented.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 from pyspark import SparkContext
 from pyspark import SparkConf
 
@@ -51,17 +52,43 @@ def process_pmc(rdd):
     from io import BytesIO
     from pmc import PMC
 
+    img_label = rdd[0].rsplit('/', 1)[1][:-6]
+
     with BytesIO(rdd[1]) as buffer:
         image = np.load(buffer)
 
     detector = PMC(ksize=(9, 9), threshold_rel=0.6)
     points = detector.get_positions(image)
 
-    return points
+    return img_label, points
 
-detected_points = sc.binaryFiles('ptv/data/*.npy').flatMap(process_pmc)
 
-detected_points.saveAsTextFile('ptv/points.txt')
+def process_relaxation(rdd):
+    from relaxation import Relaxation
+
+    img_label = rdd[0]
+
+    frame_a = rdd[1][0]
+    frame_b = rdd[1][1]
+
+    predictor = Relaxation()
+    predictor.fit(frame_a, frame_b)
+    vector_field = predictor.predict()
+
+    return img_label, vector_field
+
+
+rdd_frame_a = sc.binaryFiles('ptv/data/*a.npy').map(process_pmc)
+rdd_frame_b = sc.binaryFiles('ptv/data/*b.npy').map(process_pmc)
+
+rdd_paired_frames = rdd_frame_a.join(rdd_frame_b)
+
+res = rdd_paired_frames.map(process_relaxation)
+res.saveAsTextFile('ptv/result')
+
+
+# test = sc.binaryFiles('ptv/data/*a.npy').map(lambda rdd: rdd[0].rsplit('/', 1)[1][:-6]).saveAsTextFile('ptv/logs')
+# print(test)
 
 # from scipy.signal import correlate2d
 # import scipy
