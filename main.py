5ae443d90ca53c0472d1d005c75a0a4b1d7f5bbb
Leon Archer <leon_archer@mail.ru>
2017-05-10 11:40:16 +0700

    Intermediate changes.

--- main.py
+++ main.py
@@ -2,49 +2,9 @@
 from pyspark import SparkContext
 from pyspark import SparkConf
 
-# from io import BytesIO
-# import numpy as np
-# from scipy.misc import imread
-import os
-import sys
-
-
-# os.environ['PYSPARK_PYTHON'] = '/shared/anaconda/bin/python'
-
-
-# DATA_DIR = 'ptv/data'
-
-
-# def process_image_pmc(image):
-#     from pmc import PMC
-#
-#     pmc_obj = PMC(ksize=(7, 7), threshold_rel=0.6)
-#     coordinates = pmc_obj.get_positions(image)
-#
-#     return coordinates
-
-# def install_packages(x):
-#     import os
-#     import pip
-#
-#     basedir = os.path.abspath(os.path.dirname(__file__))
-#
-#     try:
-#         pip.main(['install', 'scipy', '-t', basedir])
-#     except:
-#         return
-#
-#     import scipy as sp
-#
-#     return ' '.join(sorted(["%s==%s" % (i.key, i.version) for i in pip.get_installed_distributions()]))
+import numpy as np
 
 sc = SparkContext()
-
-
-# from pmc import PMC
-#
-#
-# print(PMC())
 
 
 def process_pmc(rdd, kernel_size=(5, 5), threshold_rel=0.5):
@@ -84,20 +44,29 @@ def process_relaxation(rdd, R_n=30.0, R_p=30.0, R_c=1, A=0.3, B=3.0, epoch_n=1, 
             if np.sqrt((particle[2] - particle[0])**2 + (particle[3] - particle[1])**2) < max_velocity:
                 vec_field_filtered.append(particle)
 
-        vector_field = np.vstack(vec_field_filtered)
+        if vec_field_filtered:
+            vector_field = np.vstack(vec_field_filtered)
 
     return img_label, vector_field
 
 
 rdd_frame_a = sc.binaryFiles('ptv/data/*a.npy').map(lambda rdd: process_pmc(rdd, threshold_rel=0.45))
+
 rdd_frame_b = sc.binaryFiles('ptv/data/*b.npy').map(lambda rdd: process_pmc(rdd, threshold_rel=0.5))
 
-rdd_paired_frames = rdd_frame_a.join(rdd_frame_b)
+rdd_paired_frames = rdd_frame_a.join(rdd_frame_b).cache()#.repartition(sc.defaultParallelism * 3)
 
 res = rdd_paired_frames.map(lambda rdd: process_relaxation(rdd, R_n=60.0, R_p=30.0,
                                                            R_c=10, A=0.3, B=3.0,
                                                            epoch_n=6, max_velocity=20))
-res.saveAsTextFile('ptv/result')
+
+res.saveAsPickleFile('ptv/result')
+
+# rdd_test = sc.binaryFiles('ptv/data/*a.npy').map(lambda rdd: rdd[0]).saveAsTextFile('ptv/result')
+
+# for img_label, vector_field in res.toLocalIterator():
+#     path = 'result/' + img_label
+#     np.save(path, vector_field)
 
 
 # test = sc.binaryFiles('ptv/data/*a.npy').map(lambda rdd: rdd[0].rsplit('/', 1)[1][:-6]).saveAsTextFile('ptv/logs')
